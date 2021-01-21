061ddef86c45cb4c05911205fe0b893d1e273579
Tom Blauwendraat <tom@sunflowerweb.nl>
2021-01-21 14:05:27 +0000

    WIP

--- mail_edit/static/src/js/mail_edit.js
+++ mail_edit/static/src/js/mail_edit.js
@@ -3,11 +3,71 @@
  odoo.define('mail_edit.mail_edit', function (require) {
 "use strict";
 
-    var Message = require('mail.model.Message');
-    var ThreadWidget = require('mail.widget.Thread');
-    var Dialog = require('web.Dialog');
+    /*var chatter = require('mail.Chatter');
+    chatter.include({
+        start: function () {
+            var result = this._super.apply(this, arguments);
+            this.thread.on('message_forward', this, this.thread.on_message_forward);
+            return result;
+        }
+    });*/
 
-    ThreadWidget.include({
+    var Message = require('mail.model.Message');
+    var Dialog = require('web.Dialog');
+    var ThreadField = require('mail.ThreadField');
+    var ThreadWidget = require('mail.widget.Thread');
+    var session = require('web.session');
+
+    ThreadField.include({
+
+        start: function () {
+            var result = this._super.apply(this, arguments);
+            var self = this;
+            this._threadWidget.on('message_removed', this, function(message_id) {
+                // TODO: why does the message re-render after deletion
+                self._threadWidget.removeMessageAndRender(message_id, self._documentThread, {});
+            });
+            this._threadWidget.on('message_edited', this, function(message_id) {
+                /*self._documentThread._messages = _.reject(
+                    self._documentThread._messages,
+                    function(m) { return m.getID() === message_id; }
+                );*/
+                //self._documentThread._messages = [];
+                //self._fetchAndRenderThread();
+                /*var message = _.find(self._documentThread._messages, function (msg) {
+                    return msg.getID() === message_id;
+                });
+                var mailBus = self.call('mail_service', 'getMailBus');
+                mailBus.trigger('update_message', message);
+                return;*/
+
+                //var message = _.find(self._messages, function (msg) {
+                //    return msg.getID() === data.message_id;
+                //});
+                var message = self.call('mail_service', 'getMessage', message_id);
+                self._rpc({
+                    model: 'mail.message',
+                    method: 'message_format',
+                    args: [message_id],
+                    context: session.user_context,
+                })
+                .then(function (messagesData) {
+                    _.each(messagesData, function (messageData) {
+                        // TODO: call processBody for emojis
+                        // or recreate the message altogether
+                        message._body = messageData.body;
+                    });
+                    self._fetchAndRenderThread();
+                });
+
+            });
+            return result;
+        }
+
+    });
+
+     ThreadWidget.include({
+
         events: _.defaults({
             "click .o_mail_edit": "_onClickMessageEdit",
             "click .o_mail_delete": "_onClickMessageDelete",
@@ -25,14 +85,23 @@
                 },
             })
             .done(function (action) {
+                // TODO: why doesnt this return directly, like in the mail.activity wizard
+                console.log('done');
                 action.res_id = msg_id;
                 self.do_action(action, {
                     on_close: function () {
-                        location.reload();
+                        console.log('close');
+                        self.trigger("message_edited", msg_id);
+                        //location.reload();
                         //self.trigger_up('refresh_on_fly');
                         //this.reload.bind(this)
-                        //self.trigger_up('reload', { keepChanges: true });
-                         //self.trigger.bind(self, 'need_refresh')
+
+                        /*var mailBus = this.call('mail_service', 'getMailBus');
+                        mailBus.trigger('update_message', this);*/
+
+                        //self.removeMessageAndRender(msg_id, self, {});
+                        // self.trigger_up('reload', { fieldNames: ['message_ids'], keepChanges: false});
+                        //self.trigger.bind(self, 'need_refresh')
                     },
                 });
             });
@@ -46,6 +115,7 @@
             Dialog.confirm(
                 self,
                 _t("Do you really want to delete this message?"), {
+                    // TODO: why do we need to do this ourselves isnt there a function for it
                     confirm_callback: function () {
                         return self._rpc({
                             model: 'mail.message',
@@ -53,8 +123,7 @@
                             args: [[msg_id]],
                         })
                         .then(function() {
-                            //self.trigger_up('reload');
-                            location.reload();
+                            self.trigger("message_removed", msg_id);
                         });
                     },
                 }
